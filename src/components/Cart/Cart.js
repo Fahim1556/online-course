2b767c5890408a7351e628ee74b45fc00f049068
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-08-30 13:49:44 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+    const grandTotal = totalPrice.toFixed(2);
+    return (
+        <div className="fixed-bottom">
+            <Card className="text-center">
+                <Card.Body>
+                    <Card.Title><strong>Total Purchases Course:</strong> <span>$</span>{cart.length}</Card.Title>
+                    <Card.Title><strong>Total Amount:</strong> <span>$</span>{grandTotal}</Card.Title>
+                    <Button variant="primary">Confirm Purchase</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/fakeData/courses.js
@@ -0,0 +1,115 @@
+
+
+var courses =
+[
+{
+            "id":0,
+            "title": "Node.js: The Complete Guide",
+            "author": "Fahim",
+            "price":12.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1638522_fbdf.jpg"
+},
+{         
+            "id":1,
+            "title": "RESTful Web services with ASP.NET Core",
+            "author": "Mahidul",
+            "price":13.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1312650_2fc6_2.jpg"
+},
+{
+            "id":2,
+            "title": "Loopback 4: Modern ways to Build APIs in Typescript & NodeJs",
+            "author": "Amzad",
+            "price":19.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1523066_334c_13.jpg"     
+},
+{
+            "id":3,
+            "title": "Vert.x 3.5 Java API's Fast and Simple",
+            "author": "Sourov",
+            "price":15.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1657036_087a_2.jpg"
+            
+            
+},
+{
+            "id":4,
+            "title": "Learn Restful Api's with .Net Core 3.1",
+            "author": "Lokman",
+            "price":12.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/2768924_a14b_5.jpg"
+},
+{
+            "id":5,
+            "title": "ASP.NET CORE and C# REST API With Real World Projects",
+            "author": "Aual",
+            "price":14.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1257336_364b_6.jpg"
+},
+{
+            "id":6,
+            "title": "NestJs: Modern ways to build APIs with Typescript and NestJs",
+            "author": "Mahi",
+            "price":17.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/2555702_cf7b_6.jpg"
+},
+{
+            "id":7,
+            "title": "Asp.Net Core : The Complete Guide To Build Rest Api's",
+            "author": "Rakib",
+            "price":18.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/2556116_2c61_5.jpg"
+},
+{
+            "id":8,
+            "title": "Java:spring restful web service crud",
+            "author": "Rashed",
+            "price":22.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1131052_a07c.jpg"
+},
+{
+            "id":9,
+            "title": "Building RESTful APIs with Go",
+            "author": "Rifat",
+            "price":21.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/2136218_e961_4.jpg"
+},
+{
+            "id":10,
+            "title": "Learn Restful webservices/API testing with real time project",
+            "author": "Munna",
+            "price":12.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/995164_630f_2.jpg"
+},
+{
+            "id":11,
+            "title": "RESTful Web API Design with Node.js - Second Edition",
+            "author": "Mahi",
+            "price":9.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1176312_558c_2.jpg"
+},
+{
+            "id":12,
+            "title": "ASP.NET Core: Build a RESTful API - Create & Consume (2020)",
+            "author": "Sadhin",
+            "price":13.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/3403072_34bd_3.jpg"
+},
+{
+            "id":13,
+            "title": "Desenvolvendo REST / RESTful APIs com Ruby on Rails",
+            "author": "Zahid",
+            "price":15.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/1082928_b432_3.jpg"
+},
+{
+            "id":14,
+            "title": "Fastify + MongoDB, desarrolla tu propia RESTFUL API",
+            "author": "Salman",
+            "price":9.99,
+            "img":"https://img-a.udemycdn.com/course/240x135/2276501_7257.jpg"
+}
+
+];
+
+export default courses;
